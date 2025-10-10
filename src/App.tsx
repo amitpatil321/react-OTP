5ff4aee1078daa5d4927c1a232132aa3302f8faa
Amit <amitpatil321@gmail.com>
2025-10-11 00:49:51 +0530

    refactor: code refactor and css changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,110 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./App.css"
-import OtpInput from "./component/OtpInput"
+import { Hamburger } from "./component/Hamburger"
+import OtpInput from "./component/react-otp/OtpInput"
 
 function App() {
+  const [isOpen, setOpen] = useState(true)
+  const [length, setLength] = useState<number>(4)
+  const [separator, setSeparator] = useState("-")
+  const [placeholder, setPlaceholder] = useState("_")
   const [otp, setOtp] = useState<string | string[]>("")
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        setOpen(true)
+      } else setOpen(false)
+    }
+
+    window.addEventListener("resize", handleResize)
+    handleResize()
+
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
+
   return (
-    <div className="app">
-      <OtpInput
-        value="4567"
-        length={4}
-        onChange={setOtp}
-        placeholder="_"
-        disabled={false}
-        readonly={false}
-      />
-      {otp}
+    <div className="flex md:flex-row flex-col w-full h-dvh overflow-hidden">
+      <div
+        className={`absolute md:static bg-white shadow-xl w-4/5 sm:w-2/3 md:w-1/3 lg:w-1/4 xl:w-1/5 h-dvh transform transition-transform duration-300 ease-in-out z-10
+      ${isOpen ? "translate-x-0" : "-translate-x-full md:translate-x-0"}`}
+      >
+        <div className="flex justify-between items-center px-3 py-4 border-gray-200 border-b">
+          <div className="font-medium text-gray-700 text-lg">Controls</div>
+          <button
+            type="button"
+            onClick={() => setOpen(false)}
+            className="md:hidden hover:bg-gray-200 px-3 py-1 rounded-lg text-gray-600"
+          >
+            ✕
+          </button>
+        </div>
+
+        <div className="flex flex-col gap-5 p-4 controls">
+          <div>
+            <label htmlFor="length" className="block mb-1 font-medium text-gray-700 text-sm">
+              No. of Inputs
+            </label>
+            <input
+              type="number"
+              id="length"
+              placeholder="Length"
+              value={length}
+              max={10}
+              min={1}
+              onChange={(e) => setLength(Number(e.target.value))}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="separator" className="block mb-1 font-medium text-gray-700 text-sm">
+              Separator
+            </label>
+            <input
+              type="text"
+              id="separator"
+              value={separator}
+              placeholder="Separator"
+              onChange={(e) => setSeparator(e.target.value)}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="placeholder" className="block mb-1 font-medium text-gray-700 text-sm">
+              Placeholder
+            </label>
+            <input
+              type="text"
+              id="placeholder"
+              value={placeholder}
+              placeholder="Placeholder"
+              onChange={(e) => setPlaceholder(e.target.value)}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="relative flex flex-col flex-wrap justify-center items-center gap-6 px-4 py-8 w-full h-full">
+        <button
+          type="button"
+          className="md:hidden top-4 left-4 absolute hover:bg-gray-100 rounded-md"
+          onClick={() => setOpen(true)}
+        >
+          <Hamburger />
+        </button>
+
+        <OtpInput
+          value=""
+          length={length}
+          onChange={setOtp}
+          placeholder={placeholder}
+          // separator={separator}
+          disabled={false}
+          readonly={false}
+        />
+
+        <div className="font-mono text-gray-700 text-lg text-center break-all">{otp}</div>
+      </div>
     </div>
   )
 }
---
+++ src/component/Hamburger.tsx
@@ -0,0 +1,9 @@
+export const Hamburger = () => {
+  return (
+    <div className="hamburger-menu">
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
+    </div>
+  )
+}
