bac4714503bf5765e5df8b1d044567857ea2938b
Amit <amitpatil321@gmail.com>
2025-10-18 18:08:23 +0530

    feat: install storybook

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from "@eslint/js"
 import prettierConfig from "eslint-config-prettier"
 import importPlugin from "eslint-plugin-import"
@@ -10,59 +13,56 @@ import reactRefresh from "eslint-plugin-react-refresh"
 import globals from "globals"
 import tseslint from "typescript-eslint"
 
-export default tseslint.config(
-  { ignores: ["dist"] },
-  {
-    // specify the formats on which to apply the rules below
-    files: ["**/*.{ts,tsx}"],
-    // use predefined configs in installed eslint plugins
-    extends: [
-      // js
-      js.configs.recommended,
-      // ts
-      ...tseslint.configs.recommended,
-      // react
-      react.configs.flat.recommended,
-      // import
-      importPlugin.flatConfigs.recommended,
-      // a11y (accessibility
-      jsxA11y.flatConfigs.recommended,
-      // prettier
-      prettierConfig
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser
+export default tseslint.config({ ignores: ["dist"] }, {
+  // specify the formats on which to apply the rules below
+  files: ["**/*.{ts,tsx}"],
+  // use predefined configs in installed eslint plugins
+  extends: [
+    // js
+    js.configs.recommended,
+    // ts
+    ...tseslint.configs.recommended,
+    // react
+    react.configs.flat.recommended,
+    // import
+    importPlugin.flatConfigs.recommended,
+    // a11y (accessibility
+    jsxA11y.flatConfigs.recommended,
+    // prettier
+    prettierConfig
+  ],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser
+  },
+  // specify used plugins
+  plugins: {
+    "react-hooks": reactHooks,
+    "react-refresh": reactRefresh,
+    prettier: prettierPlugin
+  },
+  settings: {
+    // for eslint-plugin-react to auto detect react version
+    react: {
+      version: "detect"
     },
-    // specify used plugins
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-      prettier: prettierPlugin
-    },
-    settings: {
-      // for eslint-plugin-react to auto detect react version
-      react: {
-        version: "detect"
+    // for eslint-plugin-import to use import alias
+    "import/resolver": {
+      typescript: {
+        project: "./tsconfig.json"
       },
-      // for eslint-plugin-import to use import alias
-      "import/resolver": {
-        typescript: {
-          project: "./tsconfig.json"
-        },
-        node: {
-          extensions: [".js", ".jsx", ".ts", ".tsx", ".json", ".svg"],
-          paths: ["public"]
-        }
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".json", ".svg"],
+        paths: ["public"]
       }
-    },
-    rules: {
-      // set of custom rules
-      quotes: ["error", "double"],
-      "no-console": "warn",
-      "react/button-has-type": "error",
-      "react/react-in-jsx-scope": ["off"],
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }]
     }
+  },
+  rules: {
+    // set of custom rules
+    quotes: ["error", "double"],
+    "no-console": "warn",
+    "react/button-has-type": "error",
+    "react/react-in-jsx-scope": ["off"],
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }]
   }
-)
+}, storybook.configs["flat/recommended"]);
